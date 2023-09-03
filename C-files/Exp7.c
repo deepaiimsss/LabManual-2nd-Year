c19de882e9734190cb13339f42bc2848dee56069
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-09-03 23:31:13 +0530

    Update Exp7.c

--- C-files/Exp7.c
+++ C-files/Exp7.c
@@ -106,23 +106,21 @@ else if(data > node->data)
 node->right = del(node->right, data);
 }
 else
-{ /* Now We can delete this node and replace with either minimum element in the right sub tree or maximum element in the left subtree */
+{ 
 if(node->right && node->left)
-{ /* Here we will replace with minimum element in the right sub tree */
+{ 
  temp = findMin(node->right);
  node -> data = temp->data;
- /* As we replaced it with some other node, we have to delete that node */
  node -> right = del(node->right,temp->data);
  }
  else
  {
- /* If there is only one or zero children then we can directly remove it from the tree and connect its parent to its child */
 temp = node;
  if(node->left == NULL)
  node = node->right;
  else if(node->right == NULL)
  node = node->left;
- free(temp); /* temp is longer required */
+ free(temp); 
 }
  }
  return node;
