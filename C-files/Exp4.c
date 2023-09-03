296e121ec4bc5e8112b87608b579c7c1d850c6ce
Deepjit <127230329+deepaiimsss@users.noreply.github.com>
2023-09-03 23:26:50 +0530

    Update Exp4.c

--- C-files/Exp4.c
+++ C-files/Exp4.c
@@ -1,5 +1,4 @@
-//Design, Develop and Implement a menu driven Program in C for the following operations on
-//Singly Linked List (SLL) of Student Data with the fields: USN, Name, Branch, Sem, PhNo
+//Design, Develop and Implement a menu driven Program in C for the following operations on Singly Linked List (SLL) of Student Data with the fields: USN, Name, Branch, Sem, PhNo
 //a. Create a SLL of N Students Data by using front insertion.
 //b. Display the status of SLL and count the number of nodes in it
 //c. Perform Insertion and Deletion at End of SLL
